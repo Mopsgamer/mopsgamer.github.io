3357cc1aa160d4b9b761f928501cb98d490f3173
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2024-04-04 15:49:08 +0200

    no avatar invert, add footer, ❤️

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,64 +3,78 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'Mops',
-  tagline: 'Pugs are cool',
-  favicon: 'img/favicon.ico',
-  trailingSlash: false,
+	title: 'Mops',
+	tagline: 'Pugs are cool',
+	favicon: 'img/favicon.ico',
+	trailingSlash: false,
 
-  // Set the production url of your site here
-  url: 'https://mopsgamer.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+	// Set the production url of your site here
+	url: 'https://mopsgamer.github.io',
+	// Set the /<baseUrl>/ pathname under which your site is served
+	// For GitHub pages deployment, it is often '/<projectName>/'
+	baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Mopsgamer', // Usually your GitHub org/user name.
-  projectName: 'mopsgamer.github.io', // Usually your repo name.
-  deploymentBranch: "gh-pages",
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: 'Mopsgamer', // Usually your GitHub org/user name.
+	projectName: 'mopsgamer.github.io', // Usually your repo name.
+	deploymentBranch: "gh-pages",
 
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+	onBrokenLinks: 'warn',
+	onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+	// Even if you don't use internalization, you can use this field to set useful
+	// metadata like html lang. For example, if your site is Chinese, you may want
+	// to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: 'en',
+		locales: ['en'],
+	},
 
-  presets: [
-    [
-      'classic',
-      {
-        docs: false,
-        blog: false,
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      } satisfies Preset.Options,
-    ],
-  ],
+	presets: [
+		[
+			'classic',
+			{
+				docs: false,
+				blog: false,
+				theme: {
+					customCss: './src/css/custom.css',
+				},
+			} satisfies Preset.Options,
+		],
+	],
 
-  themeConfig: {
-    // Replace with your project's social card
-    image: 'img/mops-avatar.jpg',
-    navbar: {
-      title: 'Mops',
-      logo: {
-        alt: 'mops-avatar',
-        src: 'img/mops-avatar.jpg',
-      },
-    },
-    footer: {
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+	themeConfig: {
+		// Replace with your project's social card
+		image: 'img/mops-avatar.jpg',
+		navbar: {
+			title: 'Mops',
+			logo: {
+				alt: 'mops-avatar',
+				src: 'img/mops-avatar.jpg',
+			},
+		},
+		footer: {
+			links: [
+				{
+					title: 'Sources',
+					items: [
+						{label: 'GitHub', href: 'https://github.com/Mopsgamer/mopsgamer.github.io'}
+					]
+				},
+				{
+					title: 'Me',
+					items: [
+						{label: 'GitHub', href: 'https://github.com/Mopsgamer'}
+					]
+				}
+			]
+		},
+		prism: {
+			theme: prismThemes.github,
+			darkTheme: prismThemes.dracula,
+		},
+	} satisfies Preset.ThemeConfig,
 };
 
 export default config;
